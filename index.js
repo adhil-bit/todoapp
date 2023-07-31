bc0b0263180658ec1195389154a18da92eca6566
Adhil-bit <adhilashraf76@gmail.com>
2023-07-31 20:16:35 +0530

    db error fix

--- index.js
+++ index.js
@@ -9,20 +9,36 @@ const DB_DATABASE = process.env.DB_DATABASE;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-// Connection to MySQL database
+// Connection to MySQL database with automatic reconnection enabled
 const db = mysql.createConnection({
   user: DB_USERNAME,
   password: DB_PASSWORD,
   host: DB_HOST,
   database: DB_DATABASE,
+  multipleStatements: true, // To allow multiple statements in a single query
+  reconnect: true, // Enable automatic reconnection on connection loss
 });
 
+// Event listener for MySQL connection error
+db.on('error', (err) => {
+  console.error('MySQL connection error:', err.message);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    // Connection lost, attempt to reconnect
+    db.connect((reconnectErr) => {
+      if (reconnectErr) {
+        console.error('MySQL reconnection error:', reconnectErr.message);
+      } else {
+        console.log('---MYSQL RECONNECTED---');
+      }
+    });
+  }
+});
 
 db.connect((err) => {
-  if(err){
-      throw err;
+  if (err) {
+    throw err;
   }
-  console.log("---MYSQL CONNECTED---");
+  console.log('---MYSQL CONNECTED---');
 });
 
 app.use(express.json()); // Parse JSON request bodies
